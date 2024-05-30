2bdac01d324104c0df5c713168ac89804e6362ea
NikelMaharjan <nikelmaharjan@gmail.com>
2024-05-30 16:05:16 +0545

    changes

--- lib/home_navigation_screen.dart
+++ lib/home_navigation_screen.dart
@@ -52,6 +52,10 @@ class _HomeNavigationScreenState extends State<HomeNavigationScreen> {
             icon: Icon(Icons.night_shelter),
             label: 'nikel',
           ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.night_shelter),
+            label: 'okkkk',
+          ),
         ],
       ),
     );
